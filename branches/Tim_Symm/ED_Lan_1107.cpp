dd20f24db030128808e576bddcbb470a4f619c31
timmyson <timmyson@af8637f1-0a1e-4266-a3bc-430f206c0bf5>
2008-01-28 19:01:48 +0000

    Attempting to get commit to work

--- branches/Tim_Symm/ED_Lan_1107.cpp
+++ branches/Tim_Symm/ED_Lan_1107.cpp
@@ -2,6 +2,7 @@
 Exact Diagonalization program (Lanczos and Complete)
 for AWS' J-Q model
 Roger Melko, November 2007
+Testing Commit
 ********************************************************************/
 
 
