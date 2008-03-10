174adf8f5904bd817e6f630d1ad26570ca9ae362
timmyson <timmyson@af8637f1-0a1e-4266-a3bc-430f206c0bf5>
2008-03-10 21:05:17 +0000

    Reworking GenHam to handle Hamiltonian part Work in VectorMath.h to try to collect actual Hamiltonian calculation function and associated math functions in one file (saves on file browsing If I accumulate much in terms of math operations, I'll leave them there, otherwise it will be merged into the GenHam.cpp file.

---
+++ vectorMath.h
@@ -0,0 +1,69 @@
+//Vector Operations Library
+//Tim Foster
+//Library for inner product definition
+
+#ifndef VECTORMATH
+#define VECTORMATH
+
+
+bool innerProductStates(vector <bool> aV1, vector <bool> aV2)
+//returns a 1 if the states are identical
+{
+	bool result=true;
+
+	for (int i=0; i<aV1.size; i++)
+	{
+		if (aV1[i]!=aV2[i])
+		{result=false;}
+	}
+	return result;
+}
+
+double GENHAM::hamElementGenerator(two states)
+//wrapper for inner product across a matrix
+	double element=0; //running total of the matrix element
+	int site2; //index of the next element
+
+//calculate spin-z component - always there for states identical, just a sign issuee
+//	loop over all bonds to right and down
+	for (int i=0; i<Nsite; i++)
+	{
+		for (int j=0; j<Nsite; j++)
+		{
+			for (int dir=0; dir<2; dir++)//adding dir*Nsite will move to right or dowwn site
+			{
+				site1=i*Nsite+j;
+				if (dir==0)
+				{
+					site2=i*Nsite+( (j+1)%Nsite );//mod wraps it around
+				}
+				else
+				{
+					site2=( (i+1)%Nsite )*Nsite+j;
+				}
+				//xor them, such that true output gives 1 and false gives -1
+				element=2*state1[site1]^state1[site2]-1;
+
+				//bond flipping term
+				//test to see if up can be made down on (i,j) and partner
+				if ( (state1[site1]==1) && (state1[site2]==0))
+				{
+					//implement switching, get prefactor 1/2
+					//create temp1 the switched state
+				}
+				else if ( (state1[site1]==1) && (state1[site2]==0))
+				{
+					//implement switching, get prefactor 1/2
+					//create temp2 the switched state
+				}
+				//otherwise done
+//TODO I don't think this is a proper implementation.  Investigate further
+//TODO still need to corrent translation operator for only one-site hop in
+//Hilbert space reduction
+		}
+	}
+//multiply the whole thing by -J parameter
+
+
+#endif
+
