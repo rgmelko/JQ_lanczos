24b36afe581bf7b995725d65f6fe109508588958
Roger Melko <rgmelko@uwaterloo.ca>
2008-12-11 15:19:48 +0000

    Added #define to switch between Lanczos and HH diagonalization note: the low-lying spectrum (or 4x4 heisenberg) from HH diagonalization: -11.2284832084 -10.6498848727 -9.51768798394 -8.88644240781 x 4 -8.79437890814 x 4 -8.51828359624 x 6 -8.25282695731 x 3 -7.87790539137 x 8

--- ED_Lan_1107.cpp
+++ ED_Lan_1107.cpp
@@ -5,6 +5,7 @@ Roger Melko, November 2007
 J2 added for Complete Diagonalization only
 ********************************************************************/
 
+#define DO_LANCZOS //Lanczos or LAPACK full ED
 
 #include <fstream> 
 #include <vector> 
@@ -44,6 +45,9 @@ int main(){
   GENHAM HV(16,J,J2,Q,Sz); 
   HV.Bonds_16B(); 
 
+#ifdef DO_LANCZOS    
+  //-------------------------EVERYTHING IN THIS BLOCK OF CODE FOR LANCZOS
+
   LANCZOS lancz(HV.Vdim);  //dimension of reduced Hilbert space (Sz sector)
 
   HV.SparseHamJQ();  //generates sparse matrix Hamiltonian for Lanczos
@@ -51,22 +55,27 @@ int main(){
   lancz.Diag(HV,prm.Neigen_,prm.valvec_); // second parameter: # of eigenvalues to converge
                       // third parameter: 1 for -values only, 2 for vals AND vectors
 
-//  HV.FullHamJQ();  //generates full Sz sector Hamiltonian
+#else             
+  //-------------------EVERTHING IN THIS BLOCK OF CODE FOR FULL LAPACK ED
 
-//  cout<<"Done making the Hamiltonian \n";
-//  cout.flush();
-//  vector<double> dd;
-//  diagWithLapack_R(HV.Ham,dd);  //*** LAPACK DIAG
-//  cout<<"Done diagonalizting the Hamiltonian \n";
-//  cout.flush();
-//
-//  double min=999.0;
-//  for (int ii=0; ii<dd.size(); ii++) {
-//    cout<<setprecision(12)<<dd.at(ii)<<"\n";     
-//    if (dd.at(ii)<min) min=dd.at(ii);
-//  }
-//  cout<<endl;
-//  cout<<"HH energy: "<<min<<endl;
+  HV.FullHamJQ();  //generates full Sz sector Hamiltonian
+
+  cout<<"Done making the Hamiltonian \n";
+  cout.flush();
+  vector<double> dd;
+  diagWithLapack_R(HV.Ham,dd);  //*** LAPACK DIAG
+  cout<<"Done diagonalizting the Hamiltonian \n";
+  cout.flush();
+
+  double min=999.0;
+  for (int ii=0; ii<dd.size(); ii++) {
+    cout<<setprecision(12)<<dd.at(ii)<<"\n";     
+    if (dd.at(ii)<min) min=dd.at(ii);
+  }
+  cout<<endl;
+  cout<<"HH energy: "<<min<<endl;
+
+#endif
 
   return 0;
 
