49666da4cdad9a29c6dcb8aa154cc3c8d3cb5bf1
rgmelko <rgmelko@af8637f1-0a1e-4266-a3bc-430f206c0bf5>
2010-05-11 20:54:35 +0000

    release version for Stephen 18 site

--- branches/FF_honey_ED/ED_Lan_FFhoney.cpp
+++ branches/FF_honey_ED/ED_Lan_FFhoney.cpp
@@ -37,8 +37,8 @@ int main(){
   J=prm.JJ_;
   Sz=prm.Sz_;
 
-  GENHAM HV(24,J,Sz); 
-  HV.Bonds_24(); 
+  GENHAM HV(18,J,Sz); 
+  HV.Bonds_18p(); 
 
 #ifdef DO_LANCZOS    //EVERYTHING IN THIS BLOCK OF CODE FOR LANCZOS
 
